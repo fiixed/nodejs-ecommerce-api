75e176446bae979766f965b40917829ade710810
Andrew Bell <fiixed@gmail.com>
2023-04-18 19:13:46 -0500

    mongodb connection

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import dbConnect from '../config/dbConnect.js';
 
+dbConnect();
 const app = express();
 
 export default app;
+
---
+++ config/dbConnect.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  try {
+    const connected = mongoose.connect(
+      process.env.MONGO_URL
+    );
+    console.log(`Mongodb connected ${(await connected).connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default dbConnect;

3a87bc1edf2108a8df7fa1edc15aff9886aa3337
Andrew Bell <fiixed@gmail.com>
2023-04-18 19:29:34 -0500

    user register controller route

--- app/app.js
+++ app/app.js
@@ -2,9 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
+import userRoutes from '../routes/usersRoute.js';
 
 dbConnect();
 const app = express();
+
+//routes
+app.use('/', userRoutes);
 
 export default app;
 
--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const connected = mongoose.connect(
-      process.env.MONGO_URL
-    );
-    console.log(`Mongodb connected ${(await connected).connection.host}`);
+    const connected = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Mongodb connected ${connected.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);

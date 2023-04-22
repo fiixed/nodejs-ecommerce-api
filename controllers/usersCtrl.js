adafcbc40113486130a6888700fee4221d9541e1
Andrew Bell <fiixed@gmail.com>
2023-04-22 11:34:30 -0500

    update shipping address

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -5,13 +5,13 @@ import generateToken from "../utils/generateToken.js";
 import { getTokenFromHeader } from "../utils/getTokenFromHeader.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
-// @description Register user
-// @route POST /api/v1/users/register
-// @access Private/Admin (only dev can register Admin)
+// @desc    Register user
+// @route   POST /api/v1/users/register
+// @access  Private/Admin
 
 export const registerUserCtrl = asyncHandler(async (req, res) => {
   const { fullname, email, password } = req.body;
-  //check if user exists
+  //Check user exists
   const userExists = await User.findOne({ email });
   if (userExists) {
     //throw
@@ -28,23 +28,21 @@ export const registerUserCtrl = asyncHandler(async (req, res) => {
   });
   res.status(201).json({
     status: "success",
-    message: "User Registerd Successfully",
+    message: "User Registered Successfully",
     data: user,
   });
 });
-
-// @description Login user
-// @route POST /api/v1/users/login
-// @access Public
+// @desc    Login user
+// @route   POST /api/v1/users/login
+// @access  Public
 
 export const loginUserCtrl = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  // Find the user in db by email only
+  //Find the user in db by email only
   const userFound = await User.findOne({
     email,
   });
   if (userFound && (await bcrypt.compare(password, userFound?.password))) {
-    // same as userFound && userFound.password
     res.json({
       status: "success",
       message: "User logged in successfully",
@@ -52,19 +50,61 @@ export const loginUserCtrl = asyncHandler(async (req, res) => {
       token: generateToken(userFound?._id),
     });
   } else {
-    throw new Error("Invalid");
+    throw new Error("Invalid login credentials");
   }
 });
 
-// @description Get user profile
-// @route POST /api/v1/users/profile
-// @access Private
+// @desc    Get user profile
+// @route   GET /api/v1/users/profile
+// @access  Private
 export const getUserProfileCtrl = asyncHandler(async (req, res) => {
-  const token = getTokenFromHeader(req);
-  //verify token
-  const verified = verifyToken(token);
-  console.log(verified);
+  //find the user
+  const user = await User.findById(req.userAuthId).populate("orders");
   res.json({
-    msg: "Welcome to Profile page",
+    status: "success",
+    message: "User profile fetched successfully",
+    user,
+  });
+});
+
+// @desc    Update user shipping address
+// @route   PUT /api/v1/users/update/shipping
+// @access  Private
+
+export const updateShippingAddresCtrl = asyncHandler(async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    address,
+    city,
+    postalCode,
+    province,
+    phone,
+    country,
+  } = req.body;
+  const user = await User.findByIdAndUpdate(
+    req.userAuthId,
+    {
+      shippingAddress: {
+        firstName,
+        lastName,
+        address,
+        city,
+        postalCode,
+        province,
+        phone,
+        country,
+      },
+      hasShippingAddress: true,
+    },
+    {
+      new: true,
+    }
+  );
+  //send response
+  res.json({
+    status: "success",
+    message: "User shipping address updated successfully",
+    user,
   });
 });

874378767078631c5fd0380d4f40649ba0858b80
Andrew Bell <fiixed@gmail.com>
2023-04-22 08:35:58 -0500

    order model and dummy controller

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ import categoriesRouter from "../routes/categoriesRouter.js";
 import brandsRouter from "../routes/brandsRouter.js";
 import colorRouter from "../routes/colorRouter.js";
 import reviewRouter from "../routes/reviewRouter.js";
+import orderRouter from "../routes/ordersRouter.js";
 
 dbConnect();
 const app = express();
@@ -25,6 +26,7 @@ app.use("/api/v1/categories", categoriesRouter);
 app.use("/api/v1/brands/", brandsRouter);
 app.use("/api/v1/colors/", colorRouter);
 app.use("/api/v1/reviews/", reviewRouter);
+app.use("/api/v1/orders/", orderRouter);
 
 // error middleware
 app.use(notFound);

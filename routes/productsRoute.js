c7f1ce662316fcd750fe7f03f3fbc0bc51966e98
Andrew Bell <fiixed@gmail.com>
2023-04-27 16:21:20 -0500

    image upload in products and category

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import upload from "../config/fileUpload.js";
 import {
   createProductCtrl,
   deleteProductCtrl,
@@ -10,10 +11,10 @@ import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 const productsRouter = express.Router();
 
-productsRouter.post("/", isLoggedIn, createProductCtrl);
+productsRouter.post("/", isLoggedIn, upload.array("files"), createProductCtrl);
 productsRouter.get("/", getProductsCtrl);
 productsRouter.get("/:id", getProductCtrl);
-productsRouter.put("/:id", isLoggedIn,  updateProductCtrl);
+productsRouter.put("/:id", isLoggedIn, updateProductCtrl);
 productsRouter.delete("/:id/delete", isLoggedIn, deleteProductCtrl);
 
 export default productsRouter;

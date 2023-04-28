caa3a7358c6d53c1ff8f2d8717997529a33e17a2
Andrew Bell <fiixed@gmail.com>
2023-04-28 14:15:40 -0500

    isAdmin middleware

--- routes/colorRouter.js
+++ routes/colorRouter.js
@@ -6,14 +6,16 @@ import {
   getSingleColorCtrl,
   updateColorCtrl,
 } from "../controllers/colorsCtrl.js";
+import isAdmin from "../middlewares/isAdmin.js";
 
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 const colorRouter = exppress.Router();
 
-colorRouter.post("/", isLoggedIn, createColorCtrl);
+isAdmin;
+colorRouter.post("/", isLoggedIn, isAdmin, createColorCtrl);
 colorRouter.get("/", getAllColorsCtrl);
 colorRouter.get("/:id", getSingleColorCtrl);
-colorRouter.delete("/:id", isLoggedIn, deleteColorCtrl);
-colorRouter.put("/:id", isLoggedIn, updateColorCtrl);
+colorRouter.delete("/:id", isLoggedIn, isAdmin, deleteColorCtrl);
+colorRouter.put("/:id", isLoggedIn, isAdmin, updateColorCtrl);
 
 export default colorRouter;
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,12 +1,18 @@
 import exppress from "express";
-import { createOrderCtrl, getAllOrdersCtrl, getSingleOrderCtrl, updateOrderCtrl } from "../controllers/orderCtrl.js";
+import {
+  createOrderCtrl,
+  getSingleOrderCtrl,
+  updateOrderCtrl,
+  getOrderStatsCtrl,
+  getAllOrdersCtrl,
+} from "../controllers/orderCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 const orderRouter = exppress.Router();
 
 orderRouter.post("/", isLoggedIn, createOrderCtrl);
 orderRouter.get("/", isLoggedIn, getAllOrdersCtrl);
-orderRouter.get("/:id", isLoggedIn, getSingleOrderCtrl);
+orderRouter.get("/sales/stats", isLoggedIn, getOrderStatsCtrl);
 orderRouter.put("/update/:id", isLoggedIn, updateOrderCtrl);
-
+orderRouter.get("/:id", isLoggedIn, getSingleOrderCtrl);
 export default orderRouter;

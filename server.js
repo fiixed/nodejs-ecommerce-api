454431e4eb2e1b6a417d1fb530bb08a59715f355
Andrew Bell <fiixed@gmail.com>
2023-04-29 12:27:37 -0500

    deleted node modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import http from 'http';
-import app from './app/app.js';
+import http from "http";
+import app from "./app/app.js";
 
 //create the server
 const PORT = process.env.PORT || 9999;
 const server = http.createServer(app);
-server.listen(PORT, console.log(`Server is up and running ${PORT}`));
+server.listen(PORT, console.log(`Server is up and running boss ${PORT}`));
